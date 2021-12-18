4d30d8249b2f489d6b4470cf22a784c6cfb0381b
Ronak Agrawal <ronak.agrawal@gojek.com>
2021-12-18 14:32:11 +0530

    Deployed e86da78 with MkDocs version: 1.2.3

--- notebooks/notebook_utils.py
+++
@@ -1,8 +0,0 @@
-pd.set_option('display.max_columns', 10)
-pd.set_option('display.width', 1000)
-
-from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity = "all"
-
-import warnings
-warnings.filterwarnings('ignore')
